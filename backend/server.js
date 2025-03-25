bde1e9995e69bda3c693482f40f032427b8b0d39
syedmdfarhaneazam <syedmdfarhaneazam@gmail.com>
2025-03-25 15:42:35 +0000

    backend

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,4 @@ app.use("/crops", require("./routes/crops"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("Mongo URI:", process.env.MONGO_URI);
